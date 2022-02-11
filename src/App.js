00836dc62dc15ec291e98651da8b9abd2164bcfa
nunuji123 <nunuji123@163.com>
2022-02-11 19:00:28 +0800

    feat(css): add btn css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default function App () {
   const element = useRoutes(routes);
   return (
     <div className="App">
-      <div className="text-3xl font-bold underline">ddddddddd</div>
       {element}
     </div>
   );
--- src/component/Layout/index.js
+++ src/component/Layout/index.js
@@ -1,8 +1,44 @@
 import { Outlet, Link } from "react-router-dom";
 
+const navBarsArray = [
+    {
+        path: '/',
+        content: 'Home'
+    },
+    {
+        path: '/hook',
+        content: 'hook'
+    },
+    {
+        path: '/dom',
+        content: 'dom'
+    },
+    {
+        path: '/memo',
+        content: 'memo'
+    },
+    {
+        path: '/props',
+        content: 'props'
+    },
+]
+
 export default function Layout () {
     return (
         <div>
+            <nav>
+                <ul class="flex">
+                    {
+                        navBarsArray.map((navBar) => {
+                            return <li class="mr-6">
+                                <Link to={navBar.path}><a class="text-blue-500 hover:text-blue-800" href="#">{navBar.content}</a></Link>
+                            </li>
+                        })
+                    }
+                </ul>
+            </nav>
+            <hr />
+            <Outlet />
             <nav>
                 <ul>
                     <li>
