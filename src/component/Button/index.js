3c174ca0fa0eda4d874c8314643d4e5f80ffccb3
nunuji123 <nunuji123@163.com>
2022-02-11 11:40:46 +0800

    feat(css): add scss ; Extracting component classes from tailwind;

--- src/component/Button/index.js
+++ src/component/Button/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import "./button.css";
 
 export default class Button extends React.Component {
   constructor(props) {
     super(props);
     console.log("button theme", this.props.theme, this.props.privateStyle);
   }
-  render() {
+  render () {
     return (
       <button
-        className={this.props.theme || ""}
+        className="btn btn-blue"
+        // className={this.props.theme || ""}
         style={this.props.privateStyle || {}}
       >
         {this.props.children}
--- src/pages/hook/useState/index.js
+++ src/pages/hook/useState/index.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-export default function UseStateHook() {
+export default function UseStateHook () {
   // 声明一个叫count的state变量
   const [count, setCount] = useState(0);
   return (
     <div>
       <h6>useState</h6>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button className="btn btn-blue" onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
